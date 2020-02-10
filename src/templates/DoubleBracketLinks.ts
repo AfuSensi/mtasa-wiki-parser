43ea821a8f60e4d5682d55d6a989c9c40572097c
AfuSensi <t.amatkarijo@gmail.com>
2020-02-11 00:20:13 +0100

    Fix double bracket parser

--- src/templates/DoubleBracketLinks.ts
+++ src/templates/DoubleBracketLinks.ts
@@ -1,16 +1,8 @@
-/* New feature/item Template:
-1. Version in numeric format
-2. Version in release format
-3. Revision
-4. Description
-*/
-// // [[event system#Event source|source]]
-// Output: source
-
 const REGEX: RegExp = /\[\[.*?\|(.*?)\]\]/g;
-
+const REGEX2: RegExp = /\[\[(.{1,}?)\]\]/g;
 export function parse(input: string): string {
   // Parse template
   input = input.replace(REGEX, '$1');
+  input = input.replace(REGEX2, '$1');
   return input;
 }
